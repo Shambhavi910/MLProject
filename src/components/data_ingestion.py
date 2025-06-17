62a7cc7cd19fb81e5822352ab9ef1fd22f7b7db1
Shambhavi910 <r.shambhavi@op.iitg.ac.in>
2025-06-17 21:50:13 +0530

    datta transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split # can i go in file explorer i need to go in c?ok tq
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -44,5 +47,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion() # its ur python problem only, so what to do now ? i have installe dthe new package yesterday only
-# wait thikning  
+    train_data,test_data=obj.initiate_data_ingestion() 
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
